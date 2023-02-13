2040a6b6d4c7361ddce7b3ebe5d43b6f304e008a
MarioH88 <72152763+MarioH88@users.noreply.github.com>
2023-02-12 22:12:35 -0600

    First commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+This personal website is created using HTML, CSS and Javascript.
+
+It consists of three pages:
+
+1. Index/Home - Where I display some important things such as About Me & Skills.
+When you try to minimize the browser window it would be responsive and will have a 'hamburger' menu on the top right. When you click it a navigation menu would appear and you once you click on it, it would be bring you to that page,
+
+2. Contact Us - Where I am located and a form to contact me.
+
+3. Show off page is created HTML, CSS and Javascript. If you enter a letter, it would automatically filter it for you.For eg. If you just enter a single letter of the alphabet, it you display all entries that contain that alphabet. But if you enter the first three alphabets of the name, it would have a narrower search field.
---
+++ index.js
@@ -0,0 +1,77 @@
+// Searchable using Table header // 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".search-input").forEach((inputField) => {
+      const tableRows = inputField
+        .closest("table")
+        .querySelectorAll("tbody > tr");
+      const headerCell = inputField.closest("th");
+      const otherHeaderCells = headerCell.closest("tr").children;
+      const columnIndex = Array.from(otherHeaderCells).indexOf(headerCell);
+      const searchableCells = Array.from(tableRows).map(
+        (row) => row.querySelectorAll("td")[columnIndex]
+      );
+  
+      inputField.addEventListener("input", () => {
+        const searchQuery = inputField.value.toLowerCase();
+  
+        for (const tableCell of searchableCells) {
+          const row = tableCell.closest("tr");
+          const value = tableCell.textContent.toLowerCase().replace(",", "");
+  
+          row.style.visibility = null;
+  
+          if (value.search(searchQuery) === -1) {
+            row.style.visibility = "collapse";
+          }
+        }
+      });
+    });
+  });
+  
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (const i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , () => {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto";
+  });
+}
+  
